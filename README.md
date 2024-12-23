43628f9e14d80a6f0f94c990138835ff274fd6c0
Eason366 <ywang242@usc.edu>
2024-12-23 20:07:54 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Getting Started with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm install`
+
+Installs all the necessary node.js packages listed in the package.json
+Builds node_modules and make them ready for serving the project.
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
---
+++ src/components/LandingPage.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import './styles/landing.css';
+
+const images = [
+  require('../assets/gallery1.webp'),
+  require('../assets/gallery2.jpg'),
+  require('../assets/gallery3.webp'),
+  require('../assets/gallery4.jpg'),
+]; // Replace with your image paths
+
+const LandingPage = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000); // Change image every 3 seconds
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+      <div className="landscape-container">
+        <img src={require('../assets/la_image.jpg')} alt="Los Angeles Landscape" className="landscape" />
+      </div>
+      <div className="landing">
+        <h1>Welcome to Metro Bike Sharing Dashboard</h1>
+      </div>
+      <div className="content-container">
+        {/* Left Image Gallery */}
+        <div className="gallery-section">
+          <img
+            src={images[currentImageIndex]}
+            alt={`Slide ${currentImageIndex}`}
+            className="gallery-image"
+          />
+        </div>
+        {/* Right Description */}
+        <div className="description-section">
+        <h2>Tailoring bike routes for today's urban rider</h2>
+          <p>
+          Our dashboard designs routes that address the real challenges of urban biking, 
+          ensuring each journey is informed, safe, and tailored to the complexities of city 
+          travel. This user-focused tool goes beyond business analytics to enhance every cyclist's 
+          experience.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
